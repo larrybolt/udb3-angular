9f72be809e5aac911f4fd57bdff46d9e6d8955ab
Kristof Coomans <kristof@2dotstwice.be>
2016-02-19 15:26:37 +0100

    III-793: Use location label instead of location cdbid to search duplicate Events, otherwise there are no matches for Events entered through UDB3 because a location cdbid is not propagated to UDB2

--- src/event_form/steps/event-form-step4.controller.js
+++ src/event_form/steps/event-form-step4.controller.js
@@ -141,7 +141,7 @@ function EventFormStep4Controller($scope, EventFormData, udbApi, appConfig, Sear
       /*jscs:disable requireCamelCaseOrUpperCaseIdentifiers */
       return {
         text: EventFormData.name.nl,
-        location_cdbid : location.id
+        location_label : location.name.nl
       };
     }
     else {
