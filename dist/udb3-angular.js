3b9728ac05d2ece3fe76a6b375ac17c470a2c001
Stijn Swaanen <swaanenstijn@gmail.com>
2016-06-08 10:39:56 +0200

    III-930 grunt-contrib-jshint to 0.12.0 + lett all jshint tests pass.

--- dist/udb3-angular.js
+++ dist/udb3-angular.js
@@ -5069,14 +5069,6 @@ function OfferEditor(jobLogger, udbApi, VariationCreationJob, BaseJob, $q, varia
       deferredUpdate.resolve(false);
     }
 
-    var createVariation = function () {
-      purpose = purpose || 'personal';
-
-      udbApi
-        .createVariation(offer.apiUrl, description, purpose)
-        .then(handleCreationJob, rejectUpdate);
-    };
-
     var handleCreationJob = function (jobData) {
       var variation = angular.copy(offer);
       variation.description.nl = description;
@@ -5088,6 +5080,14 @@ function OfferEditor(jobLogger, udbApi, VariationCreationJob, BaseJob, $q, varia
         variationRepository.save(offer.id, variation);
         deferredUpdate.resolve();
       }, rejectUpdate);
+    };
+
+    var createVariation = function () {
+      purpose = purpose || 'personal';
+
+      udbApi
+        .createVariation(offer.apiUrl, description, purpose)
+        .then(handleCreationJob, rejectUpdate);
     };
 
     var editDescription = function (variation) {
@@ -13402,25 +13402,6 @@ function Search(
   $scope.currentPage = getCurrentPage();
 
   /**
-   * @param {Query} query A query object used to update the interface and result viewer.
-   */
-  var updateQuery = function (query) {
-    $scope.activeQuery = query;
-
-    if (queryBuilder.isValid(query)) {
-      var realQuery = queryBuilder.unparse(query);
-      $scope.resultViewer.queryChanged(realQuery);
-      findEvents(realQuery);
-
-      if (realQuery !== query.originalQueryString) {
-        $scope.realQuery = realQuery;
-      } else {
-        $scope.realQuery = false;
-      }
-    }
-  };
-
-  /**
    * Fires off a search for events using a plain query string or a query object.
    * @param {String|Query} query A query string or object to search with.
    */
@@ -13444,13 +13425,22 @@ function Search(
     });
   };
 
-  var label = function () {
-    var labellingQuery = $scope.resultViewer.querySelected;
+  /**
+   * @param {Query} query A query object used to update the interface and result viewer.
+   */
+  var updateQuery = function (query) {
+    $scope.activeQuery = query;
 
-    if (labellingQuery) {
-      labelActiveQuery();
-    } else {
-      labelSelection();
+    if (queryBuilder.isValid(query)) {
+      var realQuery = queryBuilder.unparse(query);
+      $scope.resultViewer.queryChanged(realQuery);
+      findEvents(realQuery);
+
+      if (realQuery !== query.originalQueryString) {
+        $scope.realQuery = realQuery;
+      } else {
+        $scope.realQuery = false;
+      }
     }
   };
 
@@ -13488,7 +13478,7 @@ function Search(
 
   function labelActiveQuery() {
     var query = $scope.activeQuery,
-        eventCount = $scope.resultViewer.totalItems;
+      eventCount = $scope.resultViewer.totalItems;
 
     if (queryBuilder.isValid(query)) {
       var modal = $uibModal.open({
@@ -13515,6 +13505,16 @@ function Search(
       $window.alert('provide a valid query to label');
     }
   }
+
+  var label = function () {
+    var labellingQuery = $scope.resultViewer.querySelected;
+
+    if (labellingQuery) {
+      labelActiveQuery();
+    } else {
+      labelSelection();
+    }
+  };
 
   function exportEvents() {
     var exportingQuery = $scope.resultViewer.querySelected,
