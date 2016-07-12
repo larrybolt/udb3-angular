b1d20a11156570ab2fa1b4e5a23e1899e0f73635
Stijn Swaanen <swaanenstijn@gmail.com>
2016-07-12 11:09:09 +0200

    III-962 min-height on where component in entry form

--- dist/udb3-angular.js
+++ dist/udb3-angular.js
@@ -16034,9 +16034,6 @@ $templateCache.put('templates/calendar-summary.directive.html',
     "          <div class=\"alert alert-danger\" role=\"alert\" ng-show=\"cityAutoCompleteError\">\n" +
     "            Er was een probleem tijdens het ophalen van de steden\n" +
     "          </div>\n" +
-    "            <p>&nbsp;</p>\n" +
-    "            <p>&nbsp;</p>\n" +
-    "            <p>&nbsp;</p>\n" +
     "        </div>\n" +
     "        <div id=\"gemeente-gekozen\" ng-show=\"selectedCity !== ''\">\n" +
     "          <span class=\"btn-chosen\" id=\"gemeente-gekozen-button\" ng-bind=\"selectedCity\"></span>\n" +
@@ -16077,9 +16074,6 @@ $templateCache.put('templates/calendar-summary.directive.html',
     "            <div class=\"alert alert-danger\" role=\"alert\" ng-show=\"locationAutoCompleteError\">\n" +
     "              Er was een probleem tijdens het ophalen van de locaties\n" +
     "            </div>\n" +
-    "              <p>&nbsp;</p>\n" +
-    "              <p>&nbsp;</p>\n" +
-    "              <p>&nbsp;</p>\n" +
     "          </div>\n" +
     "\n" +
     "          <div id=\"locatie-gekozen\" ng-show=\"selectedLocation\" >\n" +
